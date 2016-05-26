f0622e255423ec5c275fc9f1a4fc1a1e95bb3743
Jenna Zenk <zenkjenna@gmail.com>
2016-05-26 17:00:40 -0400

    pbkdf2

--- server/api/users/user.model.js
+++ server/api/users/user.model.js
@@ -4,6 +4,8 @@ var Sequelize = require('sequelize');
 
 var db = require('../../_db');
 
+var crypto = require('crypto');
+
 var User = db.define('user', {
   name: Sequelize.STRING,
   photo: {
@@ -20,6 +22,9 @@ var User = db.define('user', {
     }
   },
   password: Sequelize.STRING,
+  salt : {
+    type : Sequelize.STRING
+  },
   isAdmin: {
     type: Sequelize.BOOLEAN,
     defaultValue: false
@@ -27,6 +32,14 @@ var User = db.define('user', {
   googleId: Sequelize.STRING,
   twitterId: Sequelize.STRING,
   githubId: Sequelize.STRING
+}, {
+  hooks : {
+    beforeValidate : function(user) {
+      user.salt = crypto.randomBytes(8);
+      user.password = crypto.pbkdf2Sync(user.password, user.salt, 100000, 100, 'sha512');
+    }
+  }
+
 });
 
 module.exports = User;
