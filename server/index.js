fe4dcb83d1f7a00702ed6c98f62d8df76de5f29b
Octowl <alsaff1987@gmail.com>
2016-05-26 16:43:17 -0400

    Swtich to https

--- server/index.js
+++ server/index.js
@@ -2,11 +2,20 @@
 
 var app = require('./app');
 var db = require('./db');
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
+
+var options = {
+    cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
+    key: fs.readFileSync(path.join(__dirname, 'key.pem'))
+}
 
 var port = 8080;
-var server = app.listen(port, function (err) {
+var server = https.createServer(options, app)
+.listen(port, function (err) {
   if (err) throw err;
-  console.log('HTTP server patiently listening on port', port);
+  console.log('HTTPS server patiently listening on port', port);
   db.sync()
   .then(function () {
     console.log('Oh and btw the postgres server is totally connected, too');
